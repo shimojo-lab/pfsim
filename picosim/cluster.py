755500265b90361c3ce7a73be2ea42a901682f13
Keichi Takahashi <keichi.t@me.com>
2017-04-22 20:53:09 +0900

    WIP

--- picosim/cluster.py
+++ picosim/cluster.py
@@ -1,6 +1,23 @@
-class Cluster:
-    def __init__(self, simulator):
-        self.simulator = simulator
+import networkx as nx
 
-    def run_single_job(self, path):
-        pass
+from .host import Host
+from .switch import Switch
+
+
+class Cluster:
+    def __init__(self, graphml):
+        self.graph = nx.read_graphml(graphml)
+
+        self.hosts = {}
+        self.switches = {}
+
+        for name, attrs in self.graph.nodes_iter(data=True):
+            typ = attrs["typ"]
+
+            if typ == "host":
+                self.hosts[name] = Host(name, **attrs)
+            elif typ == "switch":
+                self.switches[name] = Switch(name, **attrs)
+
+        for u, v, attrs in self.graph.edges_iter(data=True):
+            attrs["traffic"] = 0
