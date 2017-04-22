3a89212531b796c4cb87605711299d315ccde8cf
Keichi Takahashi <keichi.t@me.com>
2017-04-22 20:52:44 +0900

    Generate job from trace file

--- picosim/job.py
+++ picosim/job.py
@@ -1,4 +1,7 @@
+import json
+import tarfile
 from enum import Enum
+from pathlib import Path
 
 
 JobStatus = Enum("JobStatus", "CREATED QUEUED RUNNING FINISHED")
@@ -25,3 +28,30 @@ class Job:
     def __repr__(self):
         return "<Job {0} np:{1} {2}>".format(self.name, self.n_procs,
                                              self.status.name)
+
+    @classmethod
+    def from_trace(cls, path):
+        tmp = {}
+
+        with tarfile.open(path, "r:gz") as tar:
+            for member in tar.getmembers():
+                if not member.isfile() or not member.name.endswith(".json"):
+                    continue
+
+                with tar.extractfile(member) as f:
+                    trace = json.load(f)
+                    rank = trace["rank"]
+                    tx_bytes = trace["tx_bytes"]
+
+                    tmp[rank] = tx_bytes
+
+        n_procs = len(tmp)
+
+        matrix = [[0 for i in range(n_procs)] for y in range(n_procs)]
+
+        for src, vec in tmp.items():
+            for dst, traffic in enumerate(vec):
+                matrix[src][dst] = traffic
+
+        name = Path(path).name
+        return cls(name, n_procs, 100.0, matrix)
