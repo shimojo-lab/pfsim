56f5e88da3c76193861ee93b678778699c9229ff
Keichi Takahashi <keichi.t@me.com>
2017-04-26 13:35:45 +0900

    Refactoring

--- picosim/cluster.py
+++ picosim/cluster.py
@@ -10,16 +10,16 @@ class Cluster:
 
         self.graph = graph
 
-        self.hosts = {}
-        self.switches = {}
+        self.hosts = []
+        self.switches = []
 
         for name, attrs in self.graph.nodes_iter(data=True):
             typ = attrs["typ"]
 
             if typ == "host":
-                self.hosts[name] = Host(name, **attrs)
+                self.hosts.append(Host(name, **attrs))
             elif typ == "switch":
-                self.switches[name] = Switch(name, **attrs)
+                self.switches.append(Switch(name, **attrs))
 
         assert issubclass(host_selector, HostSelector)
         assert issubclass(process_mapper, ProcessMapper)
@@ -27,8 +27,8 @@ class Cluster:
         self.scheduler = scheduler(simulator=simulator,
                                    selector=host_selector(),
                                    mapper=process_mapper())
-        self.router = router(graph=self.graph, hosts=self.hosts.values(),
-                             switches=self.switches.values())
+        self.router = router(graph=self.graph, hosts=self.hosts,
+                             switches=self.switches)
         self.simulator = simulator
         self.simulator.register("job.message", self._job_message)
         self.simulator.register("job.finished", self._job_finished)
@@ -51,4 +51,4 @@ class Cluster:
 
     def submit_job(self, job, time=0.0):
         self.simulator.schedule("job.submitted", job=job, time=time,
-                                hosts=self.hosts.values())
+                                hosts=self.hosts)
