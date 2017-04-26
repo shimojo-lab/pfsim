40040ff3023bf4124494c0477f7ac2bc6420ab18
Keichi Takahashi <keichi.t@me.com>
2017-04-26 23:25:43 +0900

    Add Samples class

--- picosim/statistics.py
+++ picosim/statistics.py
@@ -1,24 +1,59 @@
 from logging import getLogger
-from math import inf, sqrt
+from math import inf, nan
 
 logger = getLogger(__name__)
 
 
-class TimedSamples:
+class Samples:
     def __init__(self, name, store=True):
         self.name = name
         self.store = store
-        self.data = []
-        self.current_time = None
-        self.current_value = None
+        self.values = []
 
-        # Statistics
         self.count = 0
         self.max = -inf
         self.min = inf
         self.mean = 0.0
         self.m2 = 0.0
-        self.tmp = 0.0
+
+    def add(self, value):
+        self.max = max(self.max, value)
+        self.min = min(self.min, value)
+        self.count += 1
+
+        delta = value - self.mean
+        self.mean += delta / self.count
+        delta2 = value - self.mean
+        self.m2 += delta * delta2
+
+        if self.store:
+            self.values.append(value)
+
+    @property
+    def variance(self):
+        if self.count < 2:
+            return nan
+
+        return self.m2 / (self.count - 1)
+
+    def report(self):
+        logger.info("{0:=^80}".format(" " + self.name + "  "))
+        logger.info("Max:       {0}".format(self.max))
+        logger.info("Min:       {0}".format(self.min))
+        logger.info("Mean:      {0}".format(self.mean))
+        logger.info("Count:     {0}".format(self.count))
+        logger.info("Variance:  {0}".format(self.variance))
+
+
+class TimeSeriesSamples(Samples):
+    def __init__(self, name, store=True):
+        super().__init__(name, store)
+        self.times = []
+        self.current_time = None
+        self.current_value = None
+
+        self.t_mean = 0.0
+        self.t_m2 = 0.0
 
     def add(self, time, value):
         if self.current_time is None:
@@ -29,38 +64,31 @@ class TimedSamples:
             self.current_value = value
             return
 
-        # Update statistics
-        self.max = max(self.max, self.current_value)
-        self.min = min(self.min, self.current_value)
-        self.count += 1
-
-        delta = self.current_value - self.mean
+        delta = self.current_value - self.t_mean
         w = (time - self.current_time) / time
-        self.mean = self.mean * (1.0 - w) + self.current_value * w
-        delta2 = self.current_value - self.mean
-        self.m2 += (time - self.current_time) * delta * delta2
-
-        self.tmp += self.current_value
+        self.t_mean = self.t_mean * (1.0 - w) + self.current_value * w
+        delta2 = self.current_value - self.t_mean
+        self.t_m2 += (time - self.current_time) * delta * delta2
 
         if self.store:
-            self.data.append((self.current_time, self.current_value))
+            self.times.append(self.current_time)
+
+        super().add(self.current_value)
+
         self.current_time = time
         self.current_value = value
 
     @property
-    def variance(self):
-        return self.m2 / self.current_time
+    def t_variance(self):
+        if self.count < 2:
+            return nan
 
-    @property
-    def sd(self):
-        return sqrt(self.variance)
+        return self.t_m2 / self.current_time
 
     def report(self):
         logger.info("{0:=^80}".format(" " + self.name + "  "))
         logger.info("Max:       {0}".format(self.max))
         logger.info("Min:       {0}".format(self.min))
-        logger.info("Mean:      {0}".format(self.mean))
+        logger.info("Mean:      {0}".format(self.t_mean))
         logger.info("Count:     {0}".format(self.count))
-        logger.info("Variance:  {0}".format(self.variance))
-        logger.info("SD:        {0}".format(self.sd))
-        logger.info("Tmp:       {0}".format(self.tmp / self.count))
+        logger.info("Variance:  {0}".format(self.t_variance))
--- picosim/test_statistics.py
+++ picosim/test_statistics.py
@@ -1,22 +1,23 @@
-from math import isclose
+from math import isclose, isnan
 
-from .statistics import TimedSamples
+from .statistics import Samples, TimeSeriesSamples
 
 
 class TestTimedSamples:
     def setup(self):
-        self.samples = TimedSamples("Test Timed Samples")
+        self.samples = TimeSeriesSamples("Test Timed Samples")
 
     def test_empty(self):
-        assert self.samples.mean == 0.0
+        assert self.samples.t_mean == 0.0
+        assert isnan(self.samples.t_variance)
         assert self.samples.count == 0
 
     def test_single(self):
         self.samples.add(0.0, 1.0)
         self.samples.add(1.0, 2.0)
 
-        assert self.samples.mean == 1.0
-        assert self.samples.variance == 0.0
+        assert self.samples.t_mean == 1.0
+        assert isnan(self.samples.t_variance)
         assert self.samples.count == 1
         assert self.samples.max == 1.0
         assert self.samples.min == 1.0
@@ -31,8 +32,8 @@ class TestTimedSamples:
         variance = (1.0 * (1.0 - mean) ** 2 + 2.0 * (2.0 - mean) ** 2
                     + 3.0 * (3.0 - mean) ** 2) / (1.0 + 2.0 + 3.0)
 
-        assert isclose(self.samples.mean, mean)
-        assert isclose(self.samples.variance, variance)
+        assert isclose(self.samples.t_mean, mean)
+        assert isclose(self.samples.t_variance, variance)
         assert self.samples.count == 3
         assert self.samples.max == 3.0
         assert self.samples.min == 1.0
@@ -42,8 +43,38 @@ class TestTimedSamples:
         self.samples.add(0.0, 2.0)
         self.samples.add(1.0, 3.0)
 
-        assert self.samples.mean == 2.0
-        assert self.samples.variance == 0.0
+        assert self.samples.t_mean == 2.0
+        assert isnan(self.samples.t_variance)
         assert self.samples.count == 1
         assert self.samples.max == 2.0
         assert self.samples.min == 2.0
+
+
+class TestSamples:
+    def setup(self):
+        self.samples = Samples("Test Samples")
+
+    def test_empty(self):
+        assert self.samples.mean == 0.0
+        assert isnan(self.samples.variance)
+        assert self.samples.count == 0
+
+    def test_single(self):
+        self.samples.add(1.0)
+
+        assert self.samples.mean == 1.0
+        assert isnan(self.samples.variance)
+        assert self.samples.count == 1
+        assert self.samples.max == 1.0
+        assert self.samples.min == 1.0
+
+    def test_multiple(self):
+        self.samples.add(1.0)
+        self.samples.add(2.0)
+        self.samples.add(3.0)
+
+        assert self.samples.mean == 2.0
+        assert self.samples.variance == 1.0
+        assert self.samples.count == 3
+        assert self.samples.max == 3.0
+        assert self.samples.min == 1.0
