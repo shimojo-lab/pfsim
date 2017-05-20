193e027bccd902a6d9888ce18751c4babd7cbe45
Keichi Takahashi <keichi.t@me.com>
2017-05-20 17:34:42 +0900

    Remove picosim.util module

--- picosim/__init__.py
+++ picosim/__init__.py
@@ -10,9 +10,37 @@ Options:
 """
 
 from docopt import docopt
+import logging.config
 
 from .experiment import Experiment
-from .util import configure_logging
+
+
+def configure_logging(verbose=False):
+    loglevel = "DEBUG" if verbose else "INFO"
+
+    logging.config.dictConfig({
+        "version": 1,
+        "formatters": {
+            "color": {
+                "()": "colorlog.ColoredFormatter",
+                "format": "%(log_color)s[%(levelname)s]%(reset)s [%(name)s]: "
+                          "%(message)s",
+                "datefmt": "%H:%M:%S"
+            }
+        },
+        "handlers": {
+            "default": {
+                "class": "logging.StreamHandler",
+                "formatter": "color",
+                "stream": "ext://sys.stdout",
+            }
+        },
+        "root": {
+            "level": loglevel,
+            "handlers": ["default"],
+        },
+        "disable_existing_loggers": False
+    })
 
 
 def main():
